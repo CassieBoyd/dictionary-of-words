741a816a915981f63d96eb0f9a06dc26c26ea228
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-14 12:01:38 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Create a dictionary with key value pairs to
+represent words (key) and its definition (value)
+"""
+word_definitions = dict()
+
+word_definitions["Pugilist"] = "Fighting with the fists; boxers, usually professionals"
+word_definitions["Littoral"] = "Of or relating to the shore of a lake, sea, or ocean"
+word_definitions["Gallimaufry"] = "A hodgepodge; jumble"
+word_definitions["Lepidopterology"] = "Branch of zoology dealing with butterflies and moths"
+
+
+"""
+Add several more words and their definitions
+   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+"""
+
+"""
+Use square bracket lookup to get the definition of two
+words and output them to the console with `print()`
+"""
+
+
+"""
+Loop over the dictionary to get the following output:
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+    The definition of [WORD] is [DEFINITION]
+"""
